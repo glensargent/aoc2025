6bddbfef1e90127ab4bf3ae69770213d82d2f151
Glen Sargent <glensarg@hotmail.co.uk>
2025-12-01 19:35:28 +0000

    chore: part 2

--- src/Day01.hs
+++ src/Day01.hs
@@ -3,30 +3,31 @@ module Main where
 main :: IO ()
 main = do
   input <- readFile "inputs/day01.txt"
-  print $ part1 (lines input)
-  putStrLn $ "Part 2: " ++ part2 input
-
-
-part1 :: [String] -> Int
-part1 inputs = handleTurns (map getMotion inputs) 50 0
+  let (p1, p2) = handleTurns (map getMotion (lines input)) 50 (0, 0)
+  print p1
+  print p2
 
 type Turn = (Char, Int)
+
 getMotion :: String -> Turn
-getMotion [] = (' ', 0) -- obsolete, just getting rid of warning
-getMotion (x : xs) = (x, read xs) 
+getMotion [] = (' ', 0)
+getMotion (x : xs) = (x, read xs)
 
-handleTurns :: [Turn] -> Int -> Int -> Int
+handleTurns :: [Turn] -> Int -> (Int, Int) -> (Int, Int)
 handleTurns [] _ acc = acc
-handleTurns (turn:rest) currentNum acc
-  | currentResult == 0 = handleTurns rest currentResult (acc + 1)
-  | otherwise          = handleTurns rest currentResult acc
+handleTurns (turn : rest) currentNum (landings, total) =
+  handleTurns rest currentResult (landings', total + passes)
   where
-    currentResult = getTurnResult turn currentNum
+    (passes, currentResult) = getTurnResult turn currentNum
+    landings' = if currentResult == 0 then landings + 1 else landings
 
-getTurnResult :: Turn -> Int -> Int
-getTurnResult ('R', x) currentVal = mod (currentVal + x) 100
-getTurnResult ('L', x) currentVal = mod (currentVal - x) 100
-getTurnResult _ currentVal = currentVal
-
-part2 :: String -> String
-part2 _input = "TODO"
+getTurnResult :: Turn -> Int -> (Int, Int)
+getTurnResult ('R', x) currentVal = divMod (currentVal + x) 100
+getTurnResult ('L', x) currentVal = (passes, newPos)
+  where
+    newPos = (currentVal - x) `mod` 100
+    passes
+      | currentVal == 0 = x `div` 100
+      | x >= currentVal = 1 + (x - currentVal) `div` 100
+      | otherwise = 0
+getTurnResult _ currentVal = (0, currentVal)
