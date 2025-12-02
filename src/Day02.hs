51b1f8df96267b43a9d4fc60666c9360e46186b2
Glen Sargent <glensarg@hotmail.co.uk>
2025-12-02 20:42:34 +0000

    chore: day2 part 2

--- src/Day02.hs
+++ src/Day02.hs
@@ -7,6 +7,7 @@ main = do
   input <- splitOn "," <$> readFile "inputs/day02.txt"
   let ranges = concatMap extractRange input
   print $ sum (map read (filter checkNumber ranges) :: [Int])
+  print $ sum (map read (filter checkNumber2 ranges) :: [Int])
 
 extractRange :: String -> [String]
 extractRange input =
@@ -19,3 +20,13 @@ checkNumber input =
   left == right
   where
     (left, right) = splitAt (length input `div` 2) input
+
+checkNumber2 :: String -> Bool
+checkNumber2 input =
+  any isRepeated [1 .. length input `div` 2]
+  where
+    isRepeated len = length input `mod` len == 0 && all (== prefix) chunks
+      where
+        prefix = take len input
+        chunks = chunksOf len input
+
